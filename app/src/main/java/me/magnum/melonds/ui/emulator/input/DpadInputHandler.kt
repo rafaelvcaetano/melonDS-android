c1ca85e68b7252aa2ed1bf34382afe2e562984e6
Rafael Caetano <rafaelvcaetano@hotmail.com>
2021-02-09 21:56:00 +0000

    Review soft input buttons
    Should improve the problem in #37

--- app/src/main/java/me/magnum/melonds/ui/emulator/input/DpadInputHandler.kt
+++ app/src/main/java/me/magnum/melonds/ui/emulator/input/DpadInputHandler.kt
@@ -1,48 +1,10 @@
 package me.magnum.melonds.ui.emulator.input
 
-import android.view.MotionEvent
-import android.view.View
 import me.magnum.melonds.domain.model.Input
 
-class DpadInputHandler(inputListener: IInputListener) : BaseInputHandler(inputListener) {
-    override fun onTouch(v: View, event: MotionEvent): Boolean {
-        val relativeX = event.x / v.width
-        val relativeY = event.y / v.height
-
-        inputListener.onKeyReleased(Input.RIGHT)
-        inputListener.onKeyReleased(Input.LEFT)
-        inputListener.onKeyReleased(Input.UP)
-        inputListener.onKeyReleased(Input.DOWN)
-
-        when (event.action) {
-            MotionEvent.ACTION_DOWN, MotionEvent.ACTION_MOVE -> if (relativeX < 0.32f) {
-                if (relativeY < 0.32f) {
-                    inputListener.onKeyPress(Input.UP)
-                    inputListener.onKeyPress(Input.LEFT)
-                } else if (relativeY < 0.68f) {
-                    inputListener.onKeyPress(Input.LEFT)
-                } else {
-                    inputListener.onKeyPress(Input.DOWN)
-                    inputListener.onKeyPress(Input.LEFT)
-                }
-            } else if (relativeX < 0.68f) {
-                if (relativeY < 0.32f) {
-                    inputListener.onKeyPress(Input.UP)
-                } else if (relativeY > 0.68f) {
-                    inputListener.onKeyPress(Input.DOWN)
-                }
-            } else {
-                if (relativeY < 0.32f) {
-                    inputListener.onKeyPress(Input.UP)
-                    inputListener.onKeyPress(Input.RIGHT)
-                } else if (relativeY < 0.68f) {
-                    inputListener.onKeyPress(Input.RIGHT)
-                } else {
-                    inputListener.onKeyPress(Input.DOWN)
-                    inputListener.onKeyPress(Input.RIGHT)
-                }
-            }
-        }
-        return true
-    }
+class DpadInputHandler(inputListener: IInputListener) : MultiButtonInputHandler(inputListener) {
+    override fun getTopInput() = Input.UP
+    override fun getLeftInput() = Input.LEFT
+    override fun getBottomInput() = Input.DOWN
+    override fun getRightInput() = Input.RIGHT
 }
---
+++ app/src/main/java/me/magnum/melonds/ui/emulator/input/MultiButtonInputHandler.kt
@@ -0,0 +1,63 @@
+package me.magnum.melonds.ui.emulator.input
+
+import android.view.MotionEvent
+import android.view.View
+import me.magnum.melonds.domain.model.Input
+import me.magnum.melonds.domain.model.Point
+import kotlin.math.pow
+
+abstract class MultiButtonInputHandler(inputListener: IInputListener) : BaseInputHandler(inputListener) {
+    private var areDimensionsInitialized = false
+    private val buttonCircles = mutableListOf<ButtonCircle>()
+
+    override fun onTouch(v: View, event: MotionEvent): Boolean {
+        if (!areDimensionsInitialized) {
+            initDimensions(v.width, v.height)
+            areDimensionsInitialized = true
+        }
+
+        inputListener.onKeyReleased(getTopInput())
+        inputListener.onKeyReleased(getRightInput())
+        inputListener.onKeyReleased(getBottomInput())
+        inputListener.onKeyReleased(getLeftInput())
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN, MotionEvent.ACTION_MOVE -> {
+                buttonCircles.forEach {
+                    if (it.containsPoint(event.x, event.y)) {
+                        inputListener.onKeyPress(it.input)
+                    }
+                }
+            }
+        }
+        return true
+    }
+
+    private fun initDimensions(viewWidth: Int, viewHeight: Int) {
+        val radiusSquared = (viewWidth * 195f / 512f).pow(2)
+        val pointToLocal: (Float, Float) -> Point = { x, y ->
+            Point().apply {
+                this.x = (viewWidth * x / 512f).toInt()
+                this.y = (viewHeight * y / 512f).toInt()
+            }
+        }
+
+        // Each circle is placed on the side of each button, near the edge of the whole image. This allows for a margin of error and the circles also intersect, allowing
+        // multiple buttons to be pressed at the same time
+        buttonCircles.add(ButtonCircle(pointToLocal(512f - 16, 256f), radiusSquared, getRightInput()))
+        buttonCircles.add(ButtonCircle(pointToLocal(256f, 512f - 16), radiusSquared, getBottomInput()))
+        buttonCircles.add(ButtonCircle(pointToLocal(256f, 16f), radiusSquared, getTopInput()))
+        buttonCircles.add(ButtonCircle(pointToLocal(16f, 256f), radiusSquared, getLeftInput()))
+    }
+
+    private data class ButtonCircle(val center: Point, val radiusSquared: Float, val input: Input) {
+        fun containsPoint(pointX: Float, pointY: Float): Boolean {
+            return (pointX - center.x).pow(2) + (pointY - center.y).pow(2) <= radiusSquared
+        }
+    }
+
+    abstract fun getTopInput(): Input
+    abstract fun getLeftInput(): Input
+    abstract fun getBottomInput(): Input
+    abstract fun getRightInput(): Input
+}
