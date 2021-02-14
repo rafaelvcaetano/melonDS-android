42e6f3d6dc4b2b778c3d75ec5a1ecc594bc87369
Rafael Caetano <rafaelvcaetano@hotmail.com>
2021-02-14 17:56:46 +0000

    AR cheat max number of sections is 128 and not 127

--- app/src/main/java/me/magnum/melonds/domain/model/Cheat.kt
+++ app/src/main/java/me/magnum/melonds/domain/model/Cheat.kt
@@ -4,6 +4,6 @@ data class Cheat(val id: Long?, val name: String, val description: String?, val 
     fun isValid(): Boolean {
         // A cheat code can only have 128 parts (512 bytes). Since each part has 8 characters, we can add 1 to the length (to ensure that each part has a matching space
         // separator) and divide by 9 (part length + space separator) to calculate the total number of parts in the cheat
-        return ((code.length + 1) / 9) < 128
+        return ((code.length + 1) / 9) <= 128
     }
 }
