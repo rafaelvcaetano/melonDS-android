14af29a5da00fda5e97973b8240d8d1d630e5d1e
Rafael Caetano <rafaelvcaetano@hotmail.com>
2021-02-26 22:27:01 +0000

    Release Beta 1.2.2

--- app/src/main/java/me/magnum/melonds/ui/emulator/EmulatorViewModel.kt
+++ app/src/main/java/me/magnum/melonds/ui/emulator/EmulatorViewModel.kt
@@ -68,10 +68,6 @@ class EmulatorViewModel @ViewModelInject constructor(
         return romsRepository.getRomAtPath(path).defaultIfEmpty(Rom(path, romDocument.uri, RomConfig())).toSingle()
     }
 
-    fun getBaseEmulatorConfiguration(): EmulatorConfiguration {
-        return settingsRepository.getEmulatorConfiguration()
-    }
-
     fun getEmulatorConfigurationForRom(rom: Rom): EmulatorConfiguration {
         val baseConfiguration = settingsRepository.getEmulatorConfiguration()
         return baseConfiguration.copy(
