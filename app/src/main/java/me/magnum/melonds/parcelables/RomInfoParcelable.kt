2f8df2a1d42a4a3ac06d44d1e489e65da0f29e00
Rafael Caetano <rafaelvcaetano@hotmail.com>
2021-04-25 22:14:35 +0100

    Migrate from kotlin-android-extensions to kotlin-parcelize

--- app/src/main/java/me/magnum/melonds/parcelables/RomInfoParcelable.kt
+++ app/src/main/java/me/magnum/melonds/parcelables/RomInfoParcelable.kt
@@ -1,7 +1,7 @@
 package me.magnum.melonds.parcelables
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 import me.magnum.melonds.domain.model.RomInfo
 
 @Parcelize
