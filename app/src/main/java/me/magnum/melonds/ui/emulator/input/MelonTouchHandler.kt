c31c7a4ce5cadf33e19a43320cfc0be066b12ea0
Rafael Caetano <rafaelvcaetano@hotmail.com>
2021-05-16 22:14:52 +0100

    Simplify input handling logic

--- app/src/main/java/me/magnum/melonds/ui/emulator/input/MelonTouchHandler.kt
+++ app/src/main/java/me/magnum/melonds/ui/emulator/input/MelonTouchHandler.kt
@@ -1,21 +1,36 @@
 package me.magnum.melonds.ui.emulator.input
 
-import me.magnum.melonds.MelonEmulator.onInputDown
-import me.magnum.melonds.MelonEmulator.onInputUp
-import me.magnum.melonds.MelonEmulator.onScreenTouch
+import me.magnum.melonds.MelonEmulator
 import me.magnum.melonds.domain.model.Input
 import me.magnum.melonds.domain.model.Point
 
 class MelonTouchHandler : IInputListener {
+    private var isLidClosed = false
+
     override fun onKeyPress(key: Input) {
-        onInputDown(key)
+        if (key == Input.HINGE) {
+            handleHingePress()
+        } else {
+            MelonEmulator.onInputDown(key)
+        }
     }
 
     override fun onKeyReleased(key: Input) {
-        onInputUp(key)
+        if (key != Input.HINGE) {
+            MelonEmulator.onInputUp(key)
+        }
     }
 
     override fun onTouch(point: Point) {
-        onScreenTouch(point.x, point.y)
+        MelonEmulator.onScreenTouch(point.x, point.y)
+    }
+
+    private fun handleHingePress() {
+        isLidClosed = !isLidClosed
+        if (isLidClosed) {
+            MelonEmulator.onInputDown(Input.HINGE)
+        } else {
+            MelonEmulator.onInputUp(Input.HINGE)
+        }
     }
 }
--- app/src/main/java/me/magnum/melonds/ui/emulator/input/SingleButtonInputHandler.kt
+++ app/src/main/java/me/magnum/melonds/ui/emulator/input/SingleButtonInputHandler.kt
@@ -1,42 +1,19 @@
 package me.magnum.melonds.ui.emulator.input
 
-import android.view.HapticFeedbackConstants
 import android.view.MotionEvent
 import android.view.View
 import me.magnum.melonds.domain.model.Input
 
-class SingleButtonInputHandler(inputListener: IInputListener, private val input: Input, private val isToggle: Boolean, private val enableHapticFeedback: Boolean) : BaseInputHandler(inputListener) {
-    private var pressed = false
-    private var toggleEnabled = false
-
+class SingleButtonInputHandler(inputListener: IInputListener, private val input: Input, private val enableHapticFeedback: Boolean) : BaseInputHandler(inputListener) {
     override fun onTouch(v: View, event: MotionEvent): Boolean {
-        if (isToggle) {
-            if (event.action == MotionEvent.ACTION_DOWN && !pressed)
-                pressed = true
-            else if (event.action == MotionEvent.ACTION_UP && pressed) {
-                toggleEnabled = !toggleEnabled
-                pressed = false
-
-                if (toggleEnabled) {
-                    inputListener.onKeyPress(input)
-                } else {
-                    inputListener.onKeyReleased(input)
-                }
-
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                inputListener.onKeyPress(input)
                 if (enableHapticFeedback) {
                     performHapticFeedback(v)
                 }
             }
-        } else {
-            when (event.action) {
-                MotionEvent.ACTION_DOWN -> {
-                    inputListener.onKeyPress(input)
-                    if (enableHapticFeedback) {
-                        performHapticFeedback(v)
-                    }
-                }
-                MotionEvent.ACTION_UP -> inputListener.onKeyReleased(input)
-            }
+            MotionEvent.ACTION_UP -> inputListener.onKeyReleased(input)
         }
         return true
     }
