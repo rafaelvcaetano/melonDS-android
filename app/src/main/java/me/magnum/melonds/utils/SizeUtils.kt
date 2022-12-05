6ee47773016a421ab5ea58b49f9dfddb1f904d9d
Rafael Caetano <rafaelvcaetano@hotmail.com>
2022-12-05 22:51:14 +0000

    Move storage size units to string resources

--- app/src/main/java/me/magnum/melonds/utils/SizeUtils.kt
+++ app/src/main/java/me/magnum/melonds/utils/SizeUtils.kt
@@ -1,5 +1,7 @@
 package me.magnum.melonds.utils
 
+import android.content.Context
+import me.magnum.melonds.R
 import me.magnum.melonds.domain.model.SizeUnit
 import java.math.BigDecimal
 import java.math.RoundingMode
@@ -8,18 +10,18 @@ typealias SizeRepresentation = Pair<Double, String>
 
 object SizeUtils {
 
-    fun getBestSizeStringRepresentation(size: SizeUnit, decimalPlaces: Int = 0): String {
-        val (sizeValue, sizeUnits) = getBestSizeRepresentation(size)
+    fun getBestSizeStringRepresentation(context: Context, size: SizeUnit, decimalPlaces: Int = 0): String {
+        val (sizeValue, sizeUnits) = getBestSizeRepresentation(context, size)
         val sizeDecimal = BigDecimal(sizeValue).setScale(decimalPlaces, RoundingMode.HALF_EVEN)
         return "${sizeDecimal}${sizeUnits}"
     }
 
-    fun getBestSizeRepresentation(size: SizeUnit): SizeRepresentation {
+    fun getBestSizeRepresentation(context: Context, size: SizeUnit): SizeRepresentation {
         return when(size.toBestRepresentation()) {
-            is SizeUnit.Bytes -> size.toBytes().toDouble() to "B"
-            is SizeUnit.KB -> size.toKB() to "KB"
-            is SizeUnit.MB -> size.toMB() to "MB"
-            is SizeUnit.GB -> size.toGB() to "GB"
+            is SizeUnit.Bytes -> size.toBytes().toDouble() to context.getString(R.string.size_bytes)
+            is SizeUnit.KB -> size.toKB() to context.getString(R.string.size_kb)
+            is SizeUnit.MB -> size.toMB() to context.getString(R.string.size_mb)
+            is SizeUnit.GB -> size.toGB() to context.getString(R.string.size_gb)
         }
     }
 }
