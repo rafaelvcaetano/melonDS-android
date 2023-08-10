7ce714ba949492faf6b8c0d54a2fb6a8ba060076
Rafael Caetano <rafaelvcaetano@hotmail.com>
2023-08-10 19:01:00 +0100

    Add fallback to check if SRAM files are actually created
    Apparently, some devices return null even if files are created successfully. This attempts to circumvent that issue

--- app/src/main/java/me/magnum/melonds/impl/emulator/SramProvider.kt
+++ app/src/main/java/me/magnum/melonds/impl/emulator/SramProvider.kt
@@ -21,6 +21,17 @@ class SramProvider(
         val sramFileName = romFileName.replaceAfterLast('.', "sav", "$romFileName.sav")
 
         val sramDocument = rootDocument.findFile(sramFileName)
-        return sramDocument?.uri ?: rootDocument.createFile("*/*", sramFileName)?.uri ?: throw SramLoadException("Could not create temporary SRAM file at ${rootDocument.uri}")
+        return if (sramDocument != null) {
+            sramDocument.uri
+        } else {
+            val newSramUri = rootDocument.createFile("application/octet-stream", sramFileName)?.uri
+            if (newSramUri == null) {
+                // It looks like some devices create the file just fine but return null. As a fallback, check if the SRAM file was actually created or not
+                // Reference: https://www.ghisler.ch/board/viewtopic.php?p=370089#p370089
+                rootDocument.findFile(sramFileName)?.uri ?: throw SramLoadException("Could not create temporary SRAM file at ${rootDocument.uri}")
+            } else {
+                newSramUri
+            }
+        }
     }
 }
