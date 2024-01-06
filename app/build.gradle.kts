587e3c8c9af420e8fdcdfac28a06dcadf3a77cfe
Rafael Caetano <rafaelvcaetano@hotmail.com>
2024-01-06 18:51:43 +0000

    Update Kotlin, Gradle and Compose Compiler dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,7 @@ android {
         }
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.7"
     }
 
     flavorDimensions.add("version")
