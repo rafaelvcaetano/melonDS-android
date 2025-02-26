b84b7124d745fa79f6c237eeb0e3671302accf98
Rafael Caetano <rafaelvcaetano@hotmail.com>
2025-02-26 14:24:43 +0000

    Use NDK 28 to support 16 KB page sizes

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -2,7 +2,7 @@ object AppConfig {
     const val compileSdkVersion = 34
     const val targetSdkVersion = compileSdkVersion
     const val minSdkVersion = 24
-    const val ndkVersion = "27.2.12479018"
+    const val ndkVersion = "28.0.13004108"
 
     const val versionCode = 32
     const val versionName = "Beta 1.9.3"
