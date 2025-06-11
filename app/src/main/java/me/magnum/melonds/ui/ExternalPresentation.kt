af8d90005e5c1fd655cd8df228ce65e4d6678a58
huesos_96 <jair_mm@live.com.mx>
2025-06-11 10:27:25 -0600

    Fix Compose imports in ExternalPresentation

--- app/src/main/java/me/magnum/melonds/ui/ExternalPresentation.kt
+++ app/src/main/java/me/magnum/melonds/ui/ExternalPresentation.kt
@@ -27,6 +27,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.fillMaxSize
 import me.magnum.melonds.ui.emulator.ui.AchievementList
 import me.magnum.melonds.ui.romlist.RomListRetroAchievementsViewModel
+import androidx.compose.ui.platform.setContent
+import me.magnum.melonds.ui.theme.MelonTheme
 
 /**
  * Presentation shown on an external display. For now it only shows
